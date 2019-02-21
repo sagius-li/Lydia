172cd0fcc471e0730b26035ae0f22062f8727fd9
Jie Li <jl@ocg.de>
2019-02-21 23:35:49 +0100

    Fixed the BackingField issue in API Response

--- OCGDS/OCGDS/App_Start/WebApiConfig.cs
+++ OCGDS/OCGDS/App_Start/WebApiConfig.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json.Serialization;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
@@ -11,6 +12,7 @@ namespace OCGDS
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new DefaultContractResolver { IgnoreSerializableAttribute = true };
 
             // CORS
             var cors = new EnableCorsAttribute("*", "*", "*");
